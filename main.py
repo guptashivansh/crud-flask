f605263dfa060c38b4a278be7828a810a06c819b
Shivansh Gupta <gupta.shivansh98@gmail.com>
2019-04-19 18:36:28 +0530

    Final Commit

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'app.sqlite')
 db = SQLAlchemy(app)
 
+
+
 class Note(db.Model):
  	id 		= db.Column(db.Integer, primary_key=True)
  	title 	= db.Column(db.String(80))
