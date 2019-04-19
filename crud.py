3b10aedfc3fce6d2fe65039e3ac96dcbbf40e562
Shivansh Gupta <gupta.shivansh98@gmail.com>
2019-04-19 18:27:39 +0530

    Initial Commit

--- crud.py
+++ crud.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, request, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from sqlalchemy import desc
 import os
 
 app = Flask(__name__)
@@ -11,60 +12,129 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 
-class Note(db.Model):
- 	id 		= db.Column(db.Integer, primary_key=True)
- 	title 	= db.Column(db.String(80))
- 	body	= db.Column(db.Text)
+class User(db.Model):
+	id 			= db.Column(db.Integer, primary_key=True)
+	first_name	= db.Column(db.String(80))
+	last_name	= db.Column(db.String(80))
+	company_name= db.Column(db.String(80))
+	age			= db.Column(db.Integer)
+	city		= db.Column(db.String(80))
+	state		= db.Column(db.String(80))
+	zip			= db.Column(db.Integer)
+	email		= db.Column(db.String(80))
+	web			= db.Column(db.String(80))
 
- 	def __init__(self, title, body):
- 		 self.title = title
- 		 self.body = body
 
-class NoteSchema(ma.Schema):
+	def __init__(self, *args, **kwargs):
+		self.id = kwargs["id"]
+		self.first_name = kwargs["first_name"]
+		self.last_name = kwargs["last_name"]
+		self.company_name = kwargs["company_name"]
+		self.age = kwargs["age"]
+		self.city = kwargs["city"]
+		self.state = kwargs["state"]
+		self.zip = kwargs["zip"]
+		self.email = kwargs["email"]
+		self.web = kwargs["web"]
+
+class UserSchema(ma.Schema):
 	class Meta:
-		fields=('id','title','body')
+		fields=('id','first_name','last_name',
+				'company_name','age','city',
+				'state','zip','email','web')
+user_schema  = UserSchema()
+users_schema = UserSchema(many=True)
 
-note_schema  = NoteSchema()
-notes_schema = NoteSchema(many=True)
+@app.route('/api/users/', methods=["GET","POST"])
+def create_or_retrieve():
+	if request.method=="GET":
+		limit = 5
+		page_no = 1
+		sort = "id"
+		name = request.args.get('name')
+		order = 1
+		if 'page' in request.args:
+			page_no = request.args.get('page', type = int)
+		
+		if 'limit' in request.args:
+			limit = request.args.get('limit', type = int)
 
-@app.route('/notes/create', methods=["POST"])
-def create():
-		title 	= request.json['title']
-		body	= request.json['body']
+		if 'sort' in request.args:
+			sort = request.args.get('sort')
+			if '-' in sort:
+				sort = sort.replace("-","")
+				order = 2
+		if 'name' in request.args:
+			if  order==1:
+				all_users = User.query.filter(User.first_name.ilike(f'%{name}%')
+										|User.last_name.ilike(f'%{name}%')).order_by(sort).paginate(page_no,limit,False).items
+			else:
+				all_users = User.query.filter(User.first_name.ilike(f'%{name}%')
+										|User.last_name.ilike(f'%{name}%')).order_by(desc(sort)).paginate(page_no,limit,False).items
+		else:
+			if  order==1:
+				all_users = User.query.order_by(sort).paginate(page_no,limit,False).items
+			else:
+				all_users = User.query.order_by(desc(sort)).paginate(page_no,limit,False).items
 
+		receieved_data = users_schema.dump(all_users)
+		return jsonify(receieved_data.data)
+	elif request.method=="POST":
+		id				= request.json['id']
+		first_name		= request.json['first_name']
+		last_name		= request.json['last_name']
+		company_name	= request.json['company_name']
+		age				= request.json['age']
+		city			= request.json['city']
+		state			= request.json['state']
+		zip				= request.json['zip']
+		email			= request.json['email']
+		web				= request.json['web']
 
-		new_note = Note(title=title,body=body)
+		new_user = User(id = id,first_name = first_name,last_name = last_name,
+						company_name = company_name, age = age, city = city,
+						state = state, zip = zip, email = email, web = web)
 
-		db.session.add(new_note)
+		db.session.add(new_user)
 		db.session.commit()
+		return ('',201)
+		# return user_schema.jsonify(new_user),201
 
-		return note_schema.jsonify(new_note)
-
-@app.route('/notes/')
-def retrieve():
-	all_notes = Note.query.all()
-	receieved_data = notes_schema.dump(all_notes)
-	return jsonify(receieved_data.data)
-
-@app.route('/notes/update/<id>', methods=["POST"])
+@app.route('/api/users/<id>', methods=["GET","PUT","DELETE"])
 def update(id):
-	if request.method=="POST":
-		obj = Note.query.get(id)
-		title = request.json["title"]
-		body  = request.json["body"]
-
-		obj.title = title
-		obj.body = body
-
+	if request.method=="GET":
+		obj = User.query.get(id)
+		received = user_schema.dump(obj)
+		return jsonify(received.data)
+	elif request.method=="PUT":
+		obj = User.query.get(id)
+		req_data = request.get_json()
+		# print(req_data)
+		if 'first_name' in req_data:
+			obj.first_name		= request.json['first_name']
+		if 'last_name' in req_data:
+			obj.last_name		= request.json['last_name']
+		if 'company_name' in req_data:
+			obj.company_name		= request.json['company_name']
+		if 'age' in req_data:
+			obj.age		= request.json['age']
+		if 'city' in req_data:
+			obj.city		= request.json['city']
+		if 'state' in req_data:
+			obj.state		= request.json['state']
+		if 'zip' in req_data:
+			obj.zip		= request.json['zip']
+		if 'email' in req_data:
+			obj.email		= request.json['email']
+		if 'web' in req_data:
+			obj.web		= request.json['web']
 		db.session.commit()
-		return note_schema.jsonify(obj)
-
-@app.route('/notes/delete/<id>', methods=['DELETE'])
-def delete(id):
-	note = Note.query.get(id)
-	db.session.delete(note)
-	db.session.commit()
-	return note_schema.jsonify(note)
+		return user_schema.jsonify(obj)
+	elif request.method=="DELETE":
+		obj = User.query.get(id)
+		db.session.delete(obj)
+		db.session.commit()
+		return ('',200)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,65 @@
+To run the project:
+1. Download the project
+2. make a virtual environment and install from requirements.txt
+3. run python crud.py
+4. send the requests using postman
+
+User has following attributes:-
+ID
+First Name
+Last Name
+Company Name
+Age
+City
+State
+Zip
+Email
+Web
+
+Application should has following endpoints:-
+/api/users - GET - To list the users 
+Response with HTTP status code 200 on success
+
+
+Also, supports some query parameters:-
+page - a number for pagination
+limit - no. of items to be return, default limit is 5
+name - search user by name as substring in First Name or Last Name (Note, use substring matching algorithm/pattern to match the name)
+Sort - name of attribute, the items to be sort. By default it returns items in ascending order if  this parameter exist, and if value of parameter is prefix with ‘-’ character, then it should return item in descending order
+Sample query endpoint:- /api/users?page=1&limit=10&name=James&sort=-age
+This endpoint should return list of 10 users whose first name or last name contains substring given name and sort the users by age in descending order of page 1.
+
+
+
+/api/users - POST - To create a new user
+Request Payload should be like in json format :-
+
+Response with HTTP status code 201 on success
+	{}
+This endpoint will create a new user inside the database
+
+
+
+/api/users/{id} - GET - To get the details of a user
+Here {id} will be the id of the user in path parameter 
+Response with HTTP status code 200 on success
+	
+
+
+/api/users/{id} - PUT - To update the details of a user
+Here {id} will be the id of the user in path parameter 
+Request Payload should be like in json format for updating first name, last name and age:-
+
+{
+    "first_name": "Josephine",
+    "last_name": "Darakjy",
+    "age": 48
+}
+
+Response with HTTP status code 200 on success
+	{}
+
+/api/users/{id} - DELETE - To delete the user
+Here {id} will be the id of the user in path parameter 
+Response with HTTP status code 200 on success
+	{}
